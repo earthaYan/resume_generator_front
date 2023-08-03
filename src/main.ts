430b0626f3b352aeb235c85aec69739921469921
yanyue <yanyue@actionsky.com>
2023-08-03 15:07:11 +0800

    add comment for main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+// 通过 createApp(根组件) 创建一个新的应用实例
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+// 挂载应用
 app.mount('#app')
