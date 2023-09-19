d8a8a3b30b2201bf7bcad928badfa6a7c0caf87b
yueyueyan <yueyueyan1215@163.com>
2023-09-20 07:51:02 +0800

    modified:   Backend/repositories/migrate.go

--- Backend/repositories/migrate.go
+++ Backend/repositories/migrate.go
@@ -4,7 +4,7 @@ import "resume_backend/models"
 
 // struct->表结构
 func migrate() {
-	err := _db.Set("gorm:table_options", "charset=utf8mb4").AutoMigrate(&models.User{}, &models.Resume{}, &models.ResumeBasicInfo{}, &models.ResumeCareerTarget{}, &models.ResumeEducation{}, &models.ResumeProjectExperience{}, &models.ResumeSkill{})
+	err := _db.Set("gorm:table_options", "charset=utf8mb4").AutoMigrate(&models.User{}, &models.Resume{}, &models.ResumeBasicInfo{}, &models.ResumeCareerTarget{}, &models.ResumeEducation{}, &models.ResumeProjectExperience{}, &models.ResumeSkill{}, &models.ResumeWorkExperience{})
 	if err != nil {
 		panic(err)
 	}
